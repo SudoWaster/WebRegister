2b8baee3a01fd7460de75a7861b890511742dc15
SudoWaster <cezaryre@gmail.com>
2018-01-16 01:15:00 +0100

    init

---
+++ WebRegister-ejb/src/main/java/pl/cezaryregec/NewSessionBean.java
@@ -0,0 +1,20 @@
+package pl.cezaryregec;
+
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+
+/**
+ *
+ * @author SudoWaster
+ */
+@Stateless
+@LocalBean
+public class NewSessionBean {
+
+    public void businessMethod() {
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
